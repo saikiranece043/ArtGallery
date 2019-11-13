5ca881119a0b7686d51aadad9c18d43d6ba71800
saikiranpotti <saikiranece043@gmail.com>
2019-11-12 21:19:28 -0500

    put some comments on the functions to be more clear on what they intend to do

--- src/ArtGallery.py
+++ src/ArtGallery.py
@@ -11,6 +11,8 @@ import time
 import matplotlib.animation as ani
 from matplotlib import style
 import sys
+
+
 # point 1 and point 2 are the indexes in coords
 # point 1 and 2 are the endpoints of line segment (i.e line joining reflex vertice and one of the triangulated ploygon vertice )
 # this function checks if a line segments cuts the polygon or not
@@ -65,27 +67,27 @@ def findvisibility(vertice):
     return visibletraingles
 
 
+#This function is to store the visubility of a guard placed on a reflex vertice
+#This is to avoid calls everytime to the visibility which requires lot of computation
 def storevisibility():
     guardvis={}
     for guard in reflexvertices:
         guardvis[guard]=findvisibility(guard)
     return guardvis
 
+
 # defining the fitness of the individual
 def fitness(indv):
         totalvisbility = []
-
-
         for index, guard in enumerate(indv):
             if guard == 1:
                 totalvisbility= totalvisbility + dictvis[index]
-
-
         totalvisbility = list(dict.fromkeys(totalvisbility))
         #print("visibility of an indvidual %s %s" % (indv, len(totalvisbility)))
         return (len(totalvisbility)/len(traingles)) * (len(indv)-indv.count(1))
 
 
+#this function to rank the indviduals in the population as per the fitness values
 def rankindv(population):
 
     fitnessresults = {}
@@ -97,7 +99,7 @@ def rankindv(population):
     return sorted(fitnessresults.items(),key=operator.itemgetter(1),reverse=True)
 
 
-
+#Selecting the top parents indexes as per the fitness values
 def selectedParents(rankedpop):
     selectedParents =[]
 
@@ -109,7 +111,7 @@ def selectedParents(rankedpop):
     return selectedParents
 
 
-
+#Mating pool has the list of indviduals sorted on their fitness
 def matingpool(population,selectedParents):
     matingPool=[]
     for index in selectedParents:
@@ -118,7 +120,7 @@ def matingpool(population,selectedParents):
     return matingPool
 
 
-
+#crossover on the bottom half i.e individuals that are not part of the elizeSize
 def crossover(matingpool,elitesize):
     children =[]
 
@@ -127,6 +129,8 @@ def crossover(matingpool,elitesize):
 
     for i in range(elitesize,len(reflexvertices)):
         loop =0
+
+        #this loop is to ensure the indviduals added as children are unique
         while True:
             loop = loop+1
             if loop == 1000:
@@ -151,7 +155,7 @@ def crossover(matingpool,elitesize):
     return children
 
 
-
+#This function is to generate children from the initial population retaining the elite size of parents
 def nextgen(population,elitesize):
     #print(population)
     print("ranked individuals",rankindv(population))
@@ -168,7 +172,7 @@ def nextgen(population,elitesize):
     return nextgen,matingPool[0]
 
 
-
+#This is a draw function that is not currently used , purely used for testing
 def drawpolygon(coord):
     # repeat the first point to create a 'closed loop'
     coord.append(coord[0])
@@ -198,7 +202,7 @@ def drawpolygon(coord):
     #print("Drawing a polygon with given vertices")
 
 
-
+#This function is not used currently was created for testing
 def geneticalgo(pop,elitesize,generations):
 
     for i in range(0,generations):
@@ -213,15 +217,25 @@ def geneticalgo(pop,elitesize,generations):
 
 
 
+#Below is to setup some global variables which are essential for all the functions above
 
-
-
+'''
+Generating the coordinates of the ploygon
+'''
 # coord = [[1,1], [3,10], [1,40], [2,80],[12,100], [12,15],[40,10]]
 
 #coord = [(596, 133), (616, 207), (661, 181), (612, 284), (671, 236), (657, 269), (726, 263), (664, 289), (735, 318),(706, 347), (738, 389), (709, 401), (628, 338), (651, 396), (646, 477), (609, 383), (599, 421), (586, 386),(529, 450), (565, 349), (454, 436), (522, 343), (474, 326), (458, 313), (493, 282), (519, 269), (528, 245),(527, 217), (535, 207), (591, 274)]
 coord=generatePolygon(800,800,600,0.35,0.4,100)
 # print(coord)
 #traingles = [((591, 274), (596, 133), (616, 207)), ((616, 207), (661, 181), (612, 284)), ((612, 284), (671, 236), (657, 269)), ((657, 269), (726, 263), (664, 289)), ((664, 289), (735, 318), (706, 347)), ((706, 347), (738, 389), (709, 401)), ((706, 347), (709, 401), (628, 338)), ((628, 338), (651, 396), (646, 477)), ((628, 338), (646, 477), (609, 383)), ((609, 383), (599, 421), (586, 386)), ((586, 386), (529, 450), (565, 349)), ((565, 349), (454, 436), (522, 343)), ((522, 343), (474, 326), (458, 313)), ((522, 343), (458, 313), (493, 282)), ((522, 343), (493, 282), (519, 269)), ((528, 245), (527, 217), (535, 207)), ((528, 245), (535, 207), (591, 274)), ((591, 274), (616, 207), (612, 284)), ((612, 284), (657, 269), (664, 289)), ((664, 289), (706, 347), (628, 338)), ((628, 338), (609, 383), (586, 386)), ((628, 338), (586, 386), (565, 349)), ((565, 349), (522, 343), (519, 269)), ((565, 349), (519, 269), (528, 245)), ((565, 349), (528, 245), (591, 274)), ((565, 349), (591, 274), (612, 284)), ((612, 284), (664, 289), (628, 338)), ((612, 284), (628, 338), (565, 349))]
+
+
+
+'''
+Traingulating the polygon
+Very important bit to calculate the visibility of a given vertice
+'''
+
 
 traingles = tripy.earclip(coord)
 #print(traingles)
@@ -233,6 +247,11 @@ for traingle in traingles:
 
 
 # Array of points will be stored
+
+'''
+First creating Point objects for each vertice
+Creating a collection of vertices and instantiating polygon obj by assigning the vertices 
+'''
 vertices = []
 nv = len(coord)
 for i in coord:
@@ -244,6 +263,12 @@ reflexvertices = []
 
 totalarea = polygon.getarea(coord)
 # print("Area",totalarea)
+
+'''
+Another important bit is to find out the reflex vertices in the ploygon
+Because we are placing the guards only on reflex vertices
+'''
+
 
 
 for idx, point in enumerate(coord):
@@ -260,11 +285,22 @@ for idx, point in enumerate(coord):
 # print(reflexvertices)
 
 
+'''
+Visbility details for each vertice is stored in a global dictionary variable
+'''
+
 #created a visibility dictionary for each guard placed on a reflex vertice
 dictvis=storevisibility()
 #pop=[]
 
 print(dictvis)
+
+
+'''
+creating a GA instance ,refer to the GA class very simple class (was created for a different reason but didn't do justice)
+Something to refactor for future
+'''
+
 # creating a Genetic algorithm object with reflex vertices and no of vertices of polygon as constructor arguments
 ga = GA(reflexvertices, nv)
 #pop=ga.pop
@@ -279,12 +315,19 @@ elitesize = 4
 
 
 
-
-
+'''
+The below are matlab lib related objects necessary for plotting the graphs and points
+'''
 style.use('ggplot')
 fig = plt.figure()
 ax1= fig.add_subplot(1,1,1)
 
+
+
+'''
+This function is invoked by the animation object repeatedly (unfortunately only way to plot dynamically using pyplot)
+This function creates generations from the functions defined above and same time updates the plots based on the values in each generation
+'''
 def geneticalgo(i):
     print(ga.pop)
 
